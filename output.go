be85db19a8c8bf8d08b2f43fad3f5a6d0205121a
Grayson Koonce <grayson@uber.com>
2016-02-26 16:53:24 -0800

    clearly represent subresults

--- output.go
+++ output.go
@@ -8,9 +8,15 @@ import (
 
 // Output results to the console
 func Output(results <-chan execute.Result) {
+	passed := true
 	for result := range results {
 		for _, subResult := range result.SubResults {
+			if subResult.Status == execute.Failed {
+				passed = false
+			}
 			fmt.Println(subResult)
 		}
 	}
+
+	fmt.Println(passed)
 }
