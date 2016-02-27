fa0abdd4f79683dc8bf1bb2f0d9a1525d756200e
Grayson Koonce <grayson.koonce@gmail.com>
2016-02-27 09:44:48 -0800

    naming...

--- plan/new.go
+++ plan/new.go
@@ -4,20 +4,20 @@ package plan
 func New(config Config) Plan {
 	return Plan{
 		Config:    config,
-		TestCases: collect(config),
+		TestCases: buildTestCases(config),
 	}
 }
 
-func collect(config Config) []TestCase {
+func buildTestCases(config Config) []TestCase {
 	var cases []TestCase
 	for _, client := range config.Clients {
-		clientTestCases := createCasesForClient(client, config.Axes, make(map[string]string))
+		clientTestCases := recurseCombinations(client, config.Axes, make(map[string]string))
 		cases = append(cases, clientTestCases...)
 	}
 	return cases
 }
 
-func createCasesForClient(client string, axes []Axis, args Arguments) []TestCase {
+func recurseCombinations(client string, axes []Axis, args Arguments) []TestCase {
 	if len(axes) == 0 {
 		return []TestCase{{
 			Client:    client,
@@ -28,7 +28,7 @@ func createCasesForClient(client string, axes []Axis, args Arguments) []TestCase
 	axis := axes[0]
 	for _, p := range axis.Values {
 		args[axis.Name] = p
-		testCases = append(testCases, createCasesForClient(client, axes[1:], args)...)
+		testCases = append(testCases, recurseCombinations(client, axes[1:], args)...)
 	}
 	return testCases
 }
